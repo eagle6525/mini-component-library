66d8bbfb4d1fd03d3ed4ab9d991d863c2ce6a9aa
Wayne Pichotta <wpichotta@truecar.com>
2021-04-01 12:13:03 -0700

    New components

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,11 +2,64 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    "--innerHeight": "8px",
+    "--height": "8px",
+    "--outerRadius": "4px",
+  },
+  medium: {
+    "--innerHeight": "12px",
+    "--height": "12px",
+    "--outerRadius": "4px",
+  },
+  large: {
+    "--innerHeight": "16px",
+    "--height": "24px",
+    "--padding": "4px",
+    "--outerRadius": "8px",
+  },
+};
+const Background = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  padding: var(--padding);
+  border-radius: var(--outerRadius);
+`;
+
+const RoudedEdger = styled.div`
+  border-radius: 4px;
+  /* Hide square inner progress bar edges when near full */
+  overflow: hidden;
+`;
+
+const Progress = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${(p) => p.width + "%"};
+  height: var(--innerHeight);
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const sizeProps = SIZES[size];
+  return (
+    <>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <Background
+        style={sizeProps}
+        role="progressbar"
+        aria-valuenow={value}
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
+        <RoudedEdger>
+          <Progress style={sizeProps} width={value}></Progress>
+        </RoudedEdger>
+      </Background>
+    </>
+  );
 };
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const SelectWrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  padding: 8px;
+  padding-right: 53px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px solid -webkit-focus-ring-color;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const ChevronIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  right: 6px;
+  bottom: 0;
+  height: 24px;
+  margin: auto;
+  pointer-events: none;
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <ChevronIcon id="chevron-down" size={24} />
+      </PresentationalBit>
+    </SelectWrapper>
   );
 };
 
